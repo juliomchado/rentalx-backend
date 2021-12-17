029f1f3607bc85922198986e7d63cc8acdf5b101
juliomchado <juliocarlos00@hotmail.com>
2021-12-17 19:46:09 +0000

    add/ csv upload + parse with stream

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,21 @@
+import { parse as csvParse } from "csv-parse";
+import fs from "fs";
+
+class ImportCategoryUseCase {
+    // constructor(){}
+    execute(file: Express.Multer.File): void {
+        const stream = fs.createReadStream(file.path);
+
+        const parseFile = csvParse({
+            delimiter: ","
+        });
+
+        stream.pipe(parseFile);
+
+        parseFile.on("data", async (line) => {
+            console.log(line);
+        });
+    }
+}
+
+export { ImportCategoryUseCase };
