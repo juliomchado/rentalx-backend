7d03e73ee53b127a74baec0478210fef24ddf66a
juliomchado <juliocarlos00@hotmail.com>
2021-12-17 00:26:42 +0000

    add/ modules + repositories interface

---
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -0,0 +1,3 @@
+class ISpecificationsRepository { }
+
+export { ISpecificationsRepository };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../repositories/CategoryRepositories";
-import { CreateCategoryService } from "../services/CreateCategoryService";
+import { CategoriesRepository } from "../modules/cars/repositories/CategoryRepositories";
+import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 
