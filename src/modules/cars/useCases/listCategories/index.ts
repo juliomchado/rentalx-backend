f1ac914b4eb178eb57efc5bba049c235d5a647f3
juliomchado <juliocarlos00@hotmail.com>
2021-12-17 17:20:45 +0000

    add/ useCase specification

--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -1,4 +1,4 @@
-import { CategoriesRepository } from "../../repositories/CategoryRepositories";
+import { CategoriesRepository } from "../../repositories/implementations/CategoryRepositories";
 import { ListCategoriesController } from "./ListCategoriesController";
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
