ac0f2d923d53ca44eacc81b4cd63ff8d2f0b0b35
juliomchado <juliocarlos00@hotmail.com>
2022-01-03 23:50:37 +0000

    add/ tratative errors

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
@@ -12,14 +13,14 @@ class CreateCategoryUseCase {
     constructor(
         @inject("CategoriesRepository")
         private categoriesRepository: ICategoriesRepository
-    ) {}
+    ) { }
 
     async execute({ description, name }: IRequest): Promise<void> {
         const categoryAlreadyExists =
             await this.categoriesRepository.findByName(name);
 
         if (categoryAlreadyExists) {
-            throw new Error("Category already exists!");
+            throw new AppError("Category already exists!");
         }
 
         this.categoriesRepository.create({ name, description });
--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { ISpecificationsRepository } from "../../repositories/ISpecificationsRepository";
 
 interface IRequest {
@@ -12,14 +13,14 @@ class CreateSpecificationUseCase {
     constructor(
         @inject("SpecificationsRepository")
         private specificationRepository: ISpecificationsRepository
-    ) {}
+    ) { }
 
     async execute({ name, description }: IRequest): Promise<void> {
         const specificationAlreadyExists =
             await this.specificationRepository.findByName(name);
 
         if (specificationAlreadyExists) {
-            throw new Error("Specification already exists!");
+            throw new AppError("Specification already exists!");
         }
 
         await this.specificationRepository.create({
