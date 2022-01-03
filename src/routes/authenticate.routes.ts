147aab797cb75e9b40f3d22fb0f95880d139467e
juliomchado <juliocarlos00@hotmail.com>
2022-01-03 21:18:45 +0000

    add/ authentication

---
+++ src/routes/authenticate.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+import { AuthenticateUserController } from "../modules/accounts/useCases/authenticateUser/AuthenticateUserController";
+
+const authenticateRoutes = Router();
+
+const authenticateUserController = new AuthenticateUserController();
+
+authenticateRoutes.post("/sessions", (req, res) =>
+    authenticateUserController.handle(req, res)
+);
+
+export { authenticateRoutes };
