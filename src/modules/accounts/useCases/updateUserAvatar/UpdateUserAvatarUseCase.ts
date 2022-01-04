801dd4743a24a147bcd2eead7fe9a90767bd529c
juliomchado <juliocarlos00@hotmail.com>
2022-01-04 17:44:44 +0000

    add/ paths + fix eslint errors

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
-import { deleteFile } from "../../../../utils/file";
-import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { deleteFile } from "@utils/file";
 
 interface IRequest {
     userId: string;
@@ -13,7 +13,7 @@ class UpdateUserAvatarUseCase {
     constructor(
         @inject("UsersRepository")
         private usersRepository: IUsersRepository
-    ) { }
+    ) {}
 
     async execute({ userId, avatarFile }: IRequest): Promise<void> {
         const user = await this.usersRepository.findById(userId);
