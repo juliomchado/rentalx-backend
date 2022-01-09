10aba1bba69f2825f7b3d1ff3aff878eac10fa96
juliomchado <juliocarlos00@hotmail.com>
2022-01-09 18:49:29 +0000

    remove/ console log

--- src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -48,7 +48,6 @@ describe("List Categories Controller", () => {
         const response = await request(app).get("/categories");
 
         expect(response.status).toBe(200);
-        console.log(response.body);
         expect(response.body.length).toBe(1);
         expect(response.body[0]).toHaveProperty("id");
         expect(response.body[0].name).toEqual("Category supertest");
