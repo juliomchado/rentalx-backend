806434da3a5ddf23352010aa60be70505abd773a
juliomchado <juliocarlos00@hotmail.com>
2022-01-14 00:23:42 +0000

    add/ userMap + ref for user image

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from "multer";
 
 import uploadConfig from "@config/upload";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
+import { ProfileUserController } from "@modules/accounts/useCases/profileUserUseCase/ProfileUserController";
 import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
@@ -12,6 +13,7 @@ const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const profileUserController = new ProfileUserController();
 
 usersRoutes.post("/", (req, res) => createUserController.handle(req, res));
 
@@ -20,6 +22,10 @@ usersRoutes.patch(
     ensureAuthenticated,
     uploadAvatar.single("avatar"),
     (req, res) => updateUserAvatarController.handle(req, res)
+);
+
+usersRoutes.get("/profile", ensureAuthenticated, (req, res) =>
+    profileUserController.handle(req, res)
 );
 
 export { usersRoutes };
